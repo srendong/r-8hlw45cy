691c465e3b26ac8f6757caf4ce403405880bedf8
Samuel Rendon G <s.redon.g@gmail.com>
2019-07-02 19:07:08 -0500

    chacked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    hide: false
+  };
+
+  handleCheckOn = () => {
+    this.setState({ hide: true });
+  };
+  handleCheckOff = () => {
+    this.setState({ hide: false });
+  };
+
   render() {
+    const text = (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    );
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label>
+          <input
+            type="checkbox"
+            onClick={
+              this.state.hide === false
+                ? this.handleCheckOn
+                : this.handleCheckOff
+            }
+          />{" "}
+          Mostrar información importante
+        </label>
+        {this.state.hide === true ? text : null}
       </div>
     );
   }
